636482c5e53ef772573dfd13c1fc285cb14d253d
Baskwo <baskwo@live.ca>
2015-12-21 23:28:51 -0500

    - Update Test

--- dubpob-matrix/src/test/java/org/dubpob/matrix/AppTest.java
+++ dubpob-matrix/src/test/java/org/dubpob/matrix/AppTest.java
@@ -1,10 +1,5 @@
 package org.dubpob.matrix;
 
-import java.util.logging.Level;
-
-import org.dubpob.matrix.impl.SimpleMatrix;
-import org.dubpob.matrix.utils.MatrixHelper;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -28,23 +23,5 @@ public class AppTest
     @SuppressWarnings("rawtypes")
 	public void testApp()
     {
-    	System.out.println(Level.SEVERE.getName());
-    	String a = "1 1\n" + 
-    			   "2 -1";
-    	SimpleMatrix A = new SimpleMatrix(2,2,a);
-    	
-    	String b = "5\n" +
-    			   "6";
-    	SimpleMatrix B = new SimpleMatrix(2,1,b);
-    	
-    	TypeMatrix inverse = MatrixHelper.getInverse(A);
-    	
-    	TypeMatrix test = MatrixHelper.multiply(inverse, B);
-    	
-    	String i = ((float)11/3) + "\n" +
- 			   	   ((float)4/3) + "";
-    	SimpleMatrix I = new SimpleMatrix(2,1,i);
-    	
-        assertEquals(I.toString(), test.toString());
     }
 }
